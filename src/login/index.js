40c99afe49add680f09eb8ac32895f2055098eb6
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2022-10-31 20:53:00 -0300

    login basico

--- src/login/index.js
+++ src/login/index.js
@@ -1,12 +1,42 @@
+import React, { useState } from 'react';
 import { Text, View, TouchableOpacity, TextInput } from 'react-native';
 // https://icons.expo.fyi/
 import { MaterialIcons } from '@expo/vector-icons';
 
 import styles from './styles'; 
+import api from '../services/api';
 
 import Logo from '../../assets/compartilhados/logo';
 
 export default function Login({ navigation }) {
+
+  const [login, setLogin] = useState('carlos@email.com');
+  const [senha, setSenha] = useState('123456789');
+  const [logou, setLogou] = useState(false);
+
+  async function verLogin() {  
+    let logou = false;
+    try {
+      //const response = await api.get('/produtos?page=' + page + '&limit=9'); 
+      let dadosUsu = {
+        login, senha
+      };
+      
+      const response = await api.post('usuarios/login', dadosUsu);
+      logou = response.data.confirma; 
+
+    } catch (err) {
+        console.log('Erro: ' + err);
+    }
+
+    if (logou) {
+      navigation.navigate('Tab');
+    } else {
+      alert('Erro!');
+    }
+    
+  }
+
   return (
     <View style={styles.container}>
       <Logo />
@@ -16,7 +46,7 @@ export default function Login({ navigation }) {
       <TouchableOpacity style={styles.btnCadUsu} onPress={() => navigation.navigate('CadastroUsu')}>
         <Text style={styles.txtCadUsu}>Cadastrar usuário</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.btnEntrar} onPress={() => navigation.navigate('Tab')}>
+      <TouchableOpacity style={styles.btnEntrar} onPress={() => verLogin()}>
         <MaterialIcons name="vpn-key" size={24} color="#FAFAFA" />
         <Text style={styles.txtEntrar}>Entrar</Text>
       </TouchableOpacity>
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://10.67.23.150:3333'
+    baseURL: 'http://10.67.23.70:3333'
 });
 
 export default api;
